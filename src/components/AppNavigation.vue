19a5a7206273279db4c87bc966c06648b6bc52d0
Jacky Lui <jackymlui@live.com>
2019-07-08 21:16:20 +1000

    using vue cli vue router and vuetify

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-toolbar app color="brown darken-4" dark>
+        <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+        <v-spacer class="hidden-md-and-up"></v-spacer>
+        <v-toolbar-title >{{appTitle}}</v-toolbar-title>
+        <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+        <v-spacer class="hidden-sm-and-down"></v-spacer>
+        <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
+        <v-btn color="browwn lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+    </v-toolbar>
+</template>
+
+<script>
+export default{
+    name:'AppNavigation'
+};
+</script>
+
+<style scoped>
+</style>
